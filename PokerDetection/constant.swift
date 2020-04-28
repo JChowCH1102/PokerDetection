935858e274f39f897be17fc259ab049a924cb687
JChowCH1102 <48242411+JChowCH1102@users.noreply.github.com>
2020-04-28 16:06:18 +0800

    add constant page

---
+++ PokerDetection/constant.swift
@@ -0,0 +1,11 @@
+//
+//  constant.swift
+//  PokerDetection
+//
+//  Created by Jason Chow on 28/4/2020.
+//  Copyright © 2020 Jason Chow. All rights reserved.
+//
+
+import Foundation
+
+let POKER_DETECTION_MODEL = poker_flush_detection_1().model
